d826a805ceb15659f4b8bb8ae888f702118ffa94
Max.etman <max.etman@student.kdg.be>
2024-11-26 11:56:13 +0100

    normal ticketfactory, minder classes lijkt betere optie

--- src/main/java/ticket/TicketFactory.java
+++ src/main/java/ticket/TicketFactory.java
@@ -5,7 +5,11 @@ import person.Person;
 import java.util.List;
 import java.util.Map;
 
-public interface TicketFactory {
-    Ticket createEvenSplitTicket(int price, Person person, List<Person> peoplePaying);
-    Ticket createUnevenSplitTicket(int price, Person person, Map<Person, Integer> moneySplitMap);
+public class TicketFactory {
+    public Ticket createEvenSplitTicket(TicketType ticketType, int price, Person person, List<Person> peoplePaying){
+        return new EvenSplitTicket(ticketType, price, person,  peoplePaying);
+    }
+    public Ticket createUnevenSplitTicket(TicketType ticketType, int price, Person person, Map<Person, Integer> moneySplitMap){
+        return new UnevenSplitTicket(ticketType, price, person, moneySplitMap);
+    };
 }
