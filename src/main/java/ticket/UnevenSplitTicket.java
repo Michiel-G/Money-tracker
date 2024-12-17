452479a8c15bd50352fb29e47779d00bd3ed5847
Max.etman <max.etman@student.kdg.be>
2024-12-17 11:25:10 +0100

    removing unnecessary personPaid map

--- src/main/java/ticket/UnevenSplitTicket.java
+++ src/main/java/ticket/UnevenSplitTicket.java
@@ -8,16 +8,10 @@ import java.util.Map;
 
 public class UnevenSplitTicket extends Ticket{
     private final Map<Person, Integer> moneySplitMap;
-    private final Map<Person, Boolean> personPaidMap;
     public UnevenSplitTicket(TicketType ticketType, int price, Person person,
                              Map<Person, Integer> moneySplitMap, List<Person> peoplePaying) {
         super(ticketType, price, person, peoplePaying);
         this.moneySplitMap = moneySplitMap;
-        this.personPaidMap = new HashMap<>();
-        for (Person personLoop : moneySplitMap.keySet()) {
-            this.personPaidMap.put(personLoop, false);
-            personLoop.addDebt(person,moneySplitMap.get(personLoop));
-        }
         int totalMapMoney=0;
         for (Integer value : moneySplitMap.values()) {
             totalMapMoney+= value;
@@ -26,15 +20,6 @@ public class UnevenSplitTicket extends Ticket{
             throw new RuntimeException("Price of the ticket is not the same as the sum of money in the moneysplitmap!");
         }
 
-    }
-
-    public boolean ticketPayedOff(){
-        for (Boolean value : personPaidMap.values()) {
-            if (!value){
-                return false;
-            }
-        }
-        return true;
     }
 
     public int getMoneyOfPerson(Person person) {
